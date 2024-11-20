d8da1d99468e2982c16d8875a6415951898f6ff0
Dhar Rawal <drawal@radiantlogic.com>
2024-11-19 19:40:48 -0600

    v1.0.04: improved command name prediction to handle ambiguous commands

--- fastworkflow/_workflows/command_name_prediction/_commands/*/response_generation/command_implementation.py
+++ fastworkflow/_workflows/command_name_prediction/_commands/*/response_generation/command_implementation.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 from pydantic import BaseModel
+from semantic_router.schema import RouteChoice
 
 import fastworkflow
 from fastworkflow.session import WorkflowSnapshot
@@ -28,7 +29,19 @@ def process_command(
     route_list = [sws_route_layer.routes, current_route_layer.routes]
     rl = fastworkflow.RouteLayerRegistry.build_route_layer_from_routelayers(route_list)
 
-    command_name = rl(command).name
+    command_name = None
+    route_choice_list = rl.retrieve_multiple_routes(command)
+    if route_choice_list:
+        if len(route_choice_list) > 1:
+            route_choice_list = route_choice_list[:2]   # get the top two route choices
+            score_difference = abs(route_choice_list[0].similarity_score - route_choice_list[1].similarity_score)
+            if score_difference < 0.09:
+                error_msg = formulate_ambiguous_command_error_message(route_choice_list)
+                return OutputOfProcessCommand(error_msg=error_msg)
+            else:
+                command_name = route_choice_list[0].name
+        else:
+            command_name = route_choice_list[0].name
 
     command_parameters = CommandParameters(command_name=command_name)
     is_valid, error_msg = validate_command_name(sws, command_parameters)
@@ -60,5 +73,18 @@ def validate_command_name(
     return (
         False,
         "The command is ambiguous. Use one of the intents below in wording your command:\n"
-        f"{command_list}",
+        f"{command_list}"
+    )
+
+def formulate_ambiguous_command_error_message(route_choice_list: list[RouteChoice]) -> str:
+    command_list = (
+        "\n".join([
+            f"{route_choice.name}" 
+            for route_choice in route_choice_list
+        ])
+    )
+
+    return (
+        f"The command is ambiguous. Use one of the intents below in wording your command:\n"
+        f"{command_list}"
     )
